f9547b7459deb12c3e41f0318d637bb5e1eb1601
Ryan Burnette <ryan.burnette@gmail.com>
2018-03-27 11:12:36 -0400

    progress

--- test/index.js
+++ test/index.js
@@ -12,7 +12,7 @@ let server
 before (async function () {
   global.expect = expect
   global.browser = await puppeteer.launch({
-    headless: true,
+    headless: false,
     slowMo: 100,
     timeout: 10000,
     args: ['--no-sandbox', '--disable-setuid-sandbox']
